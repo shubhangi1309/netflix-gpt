dd0e823fa5d65215a2308f5f941fb44964c46bdc
shubhangi-modi <shubhangi.modi@globant.com>
2024-11-28 13:44:56 +0530

    28 nov

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,67 +9,100 @@ import { toggleGptSearchView } from "../utils/gptSlice";
 import { changeLanguage } from "../utils/configSlice";
 
 const Header = () => {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const user = useSelector((store) => store.user);
-    const showGptSearch = useSelector(store => store.gpt.showGptSearch)
-    console.log(showGptSearch);
-    const handleGPTSearchClick = () => {
-        // Toggle GPT search
-        dispatch(toggleGptSearchView());
-    }
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
+  console.log(showGptSearch);
+  const handleGPTSearchClick = () => {
+    // Toggle GPT search
+    dispatch(toggleGptSearchView());
+  };
 
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, (user) => {
-            if (user) {
-                // User is signed in, see docs for a list of available properties
-                // https://firebase.google.com/docs/reference/js/auth.user
-                //   const uid = user.uid;
-                //   I can get a lot of cool things from USER object
-                const { uid, email, displayName, photoURL } = user;
-                dispatch(addUser({ uid: uid, email: email, displayName: displayName, photoURL: photoURL }));
-                navigate("/browse");
-            } else {
-                // User is signed out
-                dispatch(removeUser());
-                navigate("/")
-            }
-        });
-        // unsubscribe when Component unmounts
-        return unsubscribe();
-    }, []);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        //   const uid = user.uid;
+        //   I can get a lot of cool things from USER object
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(
+          addUser({
+            uid: uid,
+            email: email,
+            displayName: displayName,
+            photoURL: photoURL,
+          })
+        );
+        navigate("/browse");
+      } else {
+        // User is signed out
+        dispatch(removeUser());
+        navigate("/");
+      }
+    });
+    // unsubscribe when Component unmounts
+    return unsubscribe();
+  }, []);
 
-    // to use the photo URL
+  // to use the photo URL
 
-    const handleSignOut = () => {
-        signOut(auth).then(() => {
-        }).catch(() => {
-        })
-    }
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {})
+      .catch(() => {});
+  };
 
-    const handleLanguageChange = (e) => {
-        dispatch(changeLanguage(e.target.value));
-    }
+  const handleLanguageChange = (e) => {
+    dispatch(changeLanguage(e.target.value));
+  };
 
-    return (
-        // <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between bg-black sm:bg-blue-900 md:bg-green-900 ">
-        <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row md:justify-between">
-            <img className="w-44 mx-auto md:mx-0" alt="logo" src={LOGO} />
-            {user && (<div className="flex p-2 justify-between">
-                {/* <img className="w-12 h-12" src="https://occ-0-6431-3647.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABeuqjuQsRgqEDlibtJTI5BMf8IxhLlLOeIT6xI4TL57mqv7XHja43gx02S8pZVe8JNGRQXjnrUk1VcsTXqi83tFKPI6OR3k.png?r=bd7" alt="user-icon"/> */}
-                <img className="w-12 h-12" src={user?.photoURL} alt="user-icon" />
-                <button onClick={handleSignOut} className="font-bold text-white">(Sign Out)</button>
-            </div>)}
-            (<div className="flex p-2">
-                {showGptSearch && (<select className="m-2 p-2 bg-gray-900 text-white" onChange={handleLanguageChange}>
-                    {SUPPORTED_LANGUAGES.map(language => <option key={language.identifier} value={language.identifier}>{language.name}</option>)}
-                </select>)}
-                    {showGptSearch}
-                <button className="py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg" onClick={handleGPTSearchClick}>{ showGptSearch ? "Homepage" : "GPT Search"}</button>
-                <img className="hidden md:block w-12 h-12" src={user?.photoURL} alt="user-icon" />
-                <button onClick={handleSignOut} className="font-bold text-white">(Sign Out)</button>
-            </div>)
+  return (
+    // <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between bg-black sm:bg-blue-900 md:bg-green-900 ">
+    <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row md:justify-between">
+      <img className="w-44 mx-auto md:mx-0" alt="logo" src={LOGO} />
+      {user && (
+        <div className="flex p-2 justify-between">
+          {/* <img className="w-12 h-12" src="https://occ-0-6431-3647.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABeuqjuQsRgqEDlibtJTI5BMf8IxhLlLOeIT6xI4TL57mqv7XHja43gx02S8pZVe8JNGRQXjnrUk1VcsTXqi83tFKPI6OR3k.png?r=bd7" alt="user-icon"/> */}
+          <img className="w-12 h-12" src={user?.photoURL} alt="user-icon" />
+          <button onClick={handleSignOut} className="font-bold text-white">
+            (Sign Out)
+          </button>
         </div>
-    )
-}
+      )}
+      (
+      <div className="flex p-2">
+        {showGptSearch && (
+          <select
+            className="m-2 p-2 bg-gray-900 text-white"
+            onChange={handleLanguageChange}
+          >
+            {SUPPORTED_LANGUAGES.map((language) => (
+              <option key={language.identifier} value={language.identifier}>
+                {language.name}
+              </option>
+            ))}
+          </select>
+        )}
+        {showGptSearch}
+        <button
+          className="py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg"
+          onClick={handleGPTSearchClick}
+        >
+          {showGptSearch ? "Homepage" : "GPT Search"}
+        </button>
+        <img
+          className="hidden md:block w-12 h-12"
+          src={user?.photoURL}
+          alt="user-icon"
+        />
+        <button onClick={handleSignOut} className="font-bold text-white">
+          (Sign Out)
+        </button>
+      </div>
+      )
+    </div>
+  );
+};
 export default Header;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,88 +1,138 @@
 import { useRef, useState } from "react";
 import Header from "./Header";
 import { validateLoginForm } from "../utils/validate";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { addUser } from "../utils/userSlice";
 import { useDispatch } from "react-redux";
 import { BG_URL, USER_AVATAR } from "../utils/constants";
 
 const Login = () => {
-    const [isSignInForm, setIsSignInForm] = useState(true);
-    const [errorMessage, setErrorMessage] = useState(null);
-    const email = useRef(null); // it will create reference
-    const password = useRef(null);
-    const name = useRef(null);
-    const dispatch = useDispatch();
+  const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const email = useRef(null); // it will create reference
+  const password = useRef(null);
+  const name = useRef(null);
+  const dispatch = useDispatch();
 
-    const toggleSignInForm = () => {
-        setIsSignInForm(!isSignInForm);
-    }
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
 
-    const handleSubmitBtnClick = () => {
-        // Validate the form data
-        const message = validateLoginForm(email.current.value, password.current.value);
-        setErrorMessage(message);
-        console.log(message);
-        if (message) return;
-        // Sign Up Logic
-        if (!isSignInForm) {
-            createUserWithEmailAndPassword(auth, email.current.value, password.current.value).then((userCredential) => {
-                const user = userCredential.user;
-                updateProfile(user, {
-                    displayName: name.current.value, 
-                    photoURL: USER_AVATAR
-                  }).then(() => {
-                    const { uid, email, displayName, photoURL } = auth.currentUser;
-                    dispatch(addUser({ uid: uid, email: email, displayName: displayName, photoURL: photoURL }));
-                  }).catch((error) => {
-                    setErrorMessage(error.message);
-                  });
+  const handleSubmitBtnClick = () => {
+    // Validate the form data
+    const message = validateLoginForm(
+      email.current.value,
+      password.current.value
+    );
+    setErrorMessage(message);
+    console.log(message);
+    if (message) return;
+    // Sign Up Logic
+    if (!isSignInForm) {
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          const user = userCredential.user;
+          updateProfile(user, {
+            displayName: name.current.value,
+            photoURL: USER_AVATAR,
+          })
+            .then(() => {
+              const { uid, email, displayName, photoURL } = auth.currentUser;
+              dispatch(
+                addUser({
+                  uid: uid,
+                  email: email,
+                  displayName: displayName,
+                  photoURL: photoURL,
+                })
+              );
             })
-                .catch((error) => {
-                    const errorCode = error.code;
-                    const errorMessage = error.message;
-                    setErrorMessage(errorCode + "- " + errorMessage);
-                })
-        }
-        else {
-            // Sign in logic
-            signInWithEmailAndPassword(auth, email.current.value, password.current.value)
-                .then((userCredential) => {
-                    // Signed in 
-                    const user = userCredential.user;
-                    console.log(user);
-
-                })
-                .catch((error) => {
-                    const errorCode = error.code;
-                    const errorMessage = error.message;
-                    setErrorMessage(errorCode + "- " + errorMessage);
-                });
-        }
+            .catch((error) => {
+              setErrorMessage(error.message);
+            });
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + "- " + errorMessage);
+        });
+    } else {
+      // Sign in logic
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + "- " + errorMessage);
+        });
     }
+  };
 
-    return (
-        <div>
-            <Header />
-            <div className="absolute">
-                <img className="h-screen object-cover" src={BG_URL} alt="logo" />
-            </div>
-            <form onSubmit={(e) => e.preventDefault()} className="w-full md:w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
-                <h1 className="font-bold text-3xl py-4">{isSignInForm ? "Sign In" : "Sign Up"}</h1>
-                {!isSignInForm && <input ref={name} type="text" placeholder="Full Name" className="p-4 my-4 mx-auto w-full bg-zinc-800 text-neutral-400" />}
-                <input ref={email} type="text" placeholder="Email Address" className="p-4 my-4 mx-auto w-full bg-zinc-800 text-neutral-400" />
-                <input ref={password} type="password" placeholder="Password" className="p-4 my-4 mx-auto w-full bg-zinc-800 text-neutral-400" />
-                <p className="text-red-500 font-bold py-2">{errorMessage}</p>
-                <button className="py-4 my-6 mx-auto bg-red-700 w-full rounded-lg" onClick={handleSubmitBtnClick}>
-                    {isSignInForm ? "Sign In" : "Sign Up"}
-                </button>
-                <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
-                    {isSignInForm ? "New to Netflix? Sign Up Now" : "Already registered? Sign In Now."}
-                </p>
-            </form>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Header />
+      <div className="absolute">
+        <img className="h-screen object-cover" src={BG_URL} alt="logo" />
+      </div>
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="w-full md:w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80"
+      >
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            ref={name}
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 mx-auto w-full bg-zinc-800 text-neutral-400"
+          />
+        )}
+        <input
+          ref={email}
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-4 mx-auto w-full bg-zinc-800 text-neutral-400"
+        />
+        <input
+          ref={password}
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 mx-auto w-full bg-zinc-800 text-neutral-400"
+        />
+        <p className="text-red-500 font-bold py-2">{errorMessage}</p>
+        <button
+          className="py-4 my-6 mx-auto bg-red-700 w-full rounded-lg"
+          onClick={handleSubmitBtnClick}
+        >
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now"
+            : "Already registered? Sign In Now."}
+        </p>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
